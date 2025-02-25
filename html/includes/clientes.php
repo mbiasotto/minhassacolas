3778521f55b5a0b8543ff231d437fdfe54ad58cf
Mauricio <mauricio@ottoai.com.br>
2025-02-25 16:08:53 -0300

    clientes e fachada - imagens

--- html/includes/clientes.php
+++ html/includes/clientes.php
@@ -6,29 +6,29 @@
         <div class="row g-5 align-items-center">
             <!-- Row 1 -->
             <div class="col-6 col-md-3 cliente">
-                <img src="./imgs/cliente01.png" alt="Company">
+                <img src="./imgs/clientes/cliente01.png" alt="Company">
             </div>
             <div class="col-6 col-md-3 cliente">
-                <img src="./imgs/cliente02.png" alt="Company">
+                <img src="./imgs/clientes/cliente02.png" alt="Company">
             </div>
             <div class="col-6 col-md-3 cliente">
-                <img src="./imgs/cliente03.png" alt="Company">
+                <img src="./imgs/clientes/cliente03.png" alt="Company">
             </div>
             <div class="col-6 col-md-3 cliente">
-                <img src="./imgs/cliente04.png" alt="Company">
+                <img src="./imgs/clientes/cliente04.png" alt="Company">
             </div>
 
             <div class="col-6 col-md-3 cliente">
-                <img src="./imgs/cliente03.png" alt="Company">
+                <img src="./imgs/clientes/cliente05.png" alt="Company">
             </div>
             <div class="col-6 col-md-3 cliente">
-                <img src="./imgs/cliente04.png" alt="Company">
+                <img src="./imgs/clientes/cliente06.png" alt="Company">
             </div>
             <div class="col-6 col-md-3 cliente">
-                <img src="./imgs/cliente02.png" alt="Company">
+                <img src="./imgs/clientes/cliente07.png" alt="Company">
             </div>
             <div class="col-6 col-md-3 cliente">
-                <img src="./imgs/cliente01.png" alt="Company">
+                <img src="./imgs/clientes/cliente08.png" alt="Company">
             </div>
             
            
